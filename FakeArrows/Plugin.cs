a130195eaa6f4ebfaaff5fc59d8b177303bdd2b5
rakso20000 <rakso20000@gmail.com>
2021-04-16 22:02:40 +0200

    Initialize project

---
+++ FakeArrows/Plugin.cs
@@ -0,0 +1,36 @@
+﻿using IPA;
+using IPALogger = IPA.Logging.Logger;
+
+namespace FakeArrows {
+	
+	[Plugin(RuntimeOptions.SingleStartInit)]
+	public class Plugin {
+		
+		internal static Plugin instance { get; private set; }
+		internal static IPALogger log { get; private set; }
+		
+		[Init]
+		public Plugin(IPALogger logger) {
+			
+			instance = this;
+			log = logger;
+			
+		}
+		
+		[OnStart]
+		public void OnApplicationStart() {
+			
+			Plugin.log.Info("OnApplicationStart");
+			
+		}
+		
+		[OnExit]
+		public void OnApplicationQuit() {
+			
+			
+			
+		}
+		
+	}
+	
+}
---
+++ FakeArrows/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FakeArrows")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FakeArrows")]
+[assembly: AssemblyCopyright("Copyright © rakso20000 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9fdf6e56-ecd8-434e-9539-26bffc2640da")]
+
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
