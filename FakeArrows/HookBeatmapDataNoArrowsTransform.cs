658012bf5f03ca40de5889e265fa18f2d42f70d8
rakso20000 <rakso20000@gmail.com>
2021-04-18 07:16:52 +0200

    Show regular Notes in NoArrows mode

---
+++ FakeArrows/HookBeatmapDataNoArrowsTransform.cs
@@ -0,0 +1,18 @@
+﻿using FakeArrows.Hooking;
+
+namespace FakeArrows {
+	
+	public class HookBeatmapDataNoArrowsTransform : Hook<BeatmapDataNoArrowsTransform> {
+		
+		[Prefix]
+		public static bool CreateTransformedData(IReadonlyBeatmapData beatmapData, ref IReadonlyBeatmapData __result) {;
+			
+			__result = beatmapData;
+			
+			return false;
+			
+		}
+		
+	}
+	
+}
